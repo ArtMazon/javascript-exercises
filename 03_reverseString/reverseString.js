1e4c0021fd40fdc8b625eed6c162e211e329ddcd
artmazon <49465717+ArtMazon@users.noreply.github.com>
2023-08-31 17:07:10 -0600

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+
+
+const reverseString = function (string) {
+
+    if (string == '') {
+        return '';
+    }
+
+    const originalStringSplit = string.split('');
+    const reversedStringsplit = [];
+
+
+    for (let i = originalStringSplit.length - 1; i > -1; i--) {
+
+        reversedStringsplit.push(originalStringSplit[i]);
+    }
+
+    return reversedStringsplit.join('');
 
 };
 
