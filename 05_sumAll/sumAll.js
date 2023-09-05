25a588957bbbf5e63024aaa56f95a92d4aa9cf02
artmazon <49465717+ArtMazon@users.noreply.github.com>
2023-09-05 06:38:06 -0600

    Finished the exercise 05 and added a little test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(intialValue, finalValue) {
+
+    if(intialValue < 0 || finalValue < 0){
+        return "ERROR";
+    }
+
+    if(!(typeof(intialValue) == "number")){
+        return "ERROR";
+    }
+    if(!(typeof(finalValue) == "number")){
+        return "ERROR";
+    }
+
+    if(finalValue < intialValue){
+        let pivot = intialValue;
+        intialValue = finalValue;
+        finalValue = pivot;
+    }
+
+return Math.abs(sumOfFirstNthNumbers(finalValue)-sumOfFirstNthNumbers(intialValue-1));
+
+
 
 };
+
+
+function sumOfFirstNthNumbers(number) {
+
+    return (number*(number+1))/2;
+    
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
