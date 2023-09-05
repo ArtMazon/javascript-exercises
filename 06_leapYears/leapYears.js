b2a3ca14b706a72f4791f18d99b278f19ccd4eab
artmazon <49465717+ArtMazon@users.noreply.github.com>
2023-09-05 06:52:52 -0600

    Finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,35 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+
+    if(divisibleBy(year,4)){
+        if(divisibleBy(year,100)){
+            if(divisibleBy(year,400)){
+                return true
+            }else{
+                return false
+            }
+
+        }else{
+            return true
+        }
+
+
+    }else{
+        return false;
+    }
+
+
 
 };
+
+
+function divisibleBy(number,value) {
+
+    return number % value == 0;
+    
+}
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
