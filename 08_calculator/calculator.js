e9e2169d206385cd167bda6876af2565cb40b0b2
ArtMaz <49465717+ArtMazon@users.noreply.github.com>
2023-10-18 20:33:03 -0600

    Finished exercise number 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,51 @@
 const add = function() {
-	
+
+  return arguments[0]+arguments[1];
+
 };
 
 const subtract = function() {
+  return arguments[0]-arguments[1];
 	
 };
 
 const sum = function() {
+
+  if(arguments[0].length ==0){
+    return 0;
+  }
+
+  return arguments[0].reduce((sum,current)=>sum+current);
+
+
 	
 };
 
 const multiply = function() {
 
+  return arguments[0].reduce((multiplication,current) => multiplication*current);
+
 };
 
 const power = function() {
+
+  return arguments[0]**arguments[1];
 	
 };
 
 const factorial = function() {
+
+  
+  if(arguments[0] == 0){
+    return 1;
+  }
+
+  let factorial = 1;
+  for(let i = arguments[0]; i > 1 ; i-- ){
+    factorial *= i;
+  }
+
+  return factorial;
 	
 };
 
