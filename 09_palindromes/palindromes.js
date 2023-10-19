987993eb983f1fd8c08a7e02bf5922427cc4474c
ArtMaz <49465717+ArtMazon@users.noreply.github.com>
2023-10-18 21:36:48 -0600

    Finished exercise number 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
 const palindromes = function () {
 
+    let splitString = arguments[0].toLowerCase().split("");
+
+    let filteredValues ="!,. ";
+
+
+
+
+    let filteredSplitString = splitString.filter((character) => !filteredValues.includes(character) )
+
+
+    let palindromeCount;
+
+    if(splitString.length % 2 == 0){
+        palindromeCount = splitString.length / 2;
+    }else{
+        palindromeCount= (splitString.length-1)/2;
+    }
+
+    for(let i = 0; i < palindromeCount; i++){
+
+        if(!(filteredSplitString.at(i) === filteredSplitString.at(-i-1))){
+            return false;
+        }
+    }
+
+  /*   console.log(splitString);
+    console.log(filteredSplitString); */
+
+    return true;
+
 };
 
 // Do not edit below this line
