84189b42e6914c1f501fb06b518c1b2736749c0a
ArtMaz <49465717+ArtMazon@users.noreply.github.com>
2023-10-19 17:26:14 -0600

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+
+
+    let personAges = array.map(person =>{
+        if(person.yearOfDeath == undefined){
+            person.yearOfDeath = 2023;
+        }
+
+        return person.yearOfDeath - person.yearOfBirth;
+    })
+
+
+    oldestAge = 0;
+
+    for (const age of personAges) {
+
+        if(age > oldestAge){
+            oldestAge = age;
+        }
+        
+    }
+
+    oldestPersonIndex = personAges.indexOf(oldestAge);
+
+
+
+    return array[oldestPersonIndex];
 
 };
 
